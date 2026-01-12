3b300c98d3a637994721ca6c6af066df6dcbfa48
Tanish <tanishbhavsar2007@gmail.com>
2026-01-12 23:51:06 +0530

    change

--- display/test_display.py
+++ display/test_display.py
@@ -2,7 +2,7 @@ import time
 from luma.core.interface.serial import spi
 from luma.lcd.device import st7789
 
-from display.roboeyes.eyes import RoboEyes
+from display.eyes import RoboEyes
 
 # SPI CONFIG — match your wiring
 serial = spi(
